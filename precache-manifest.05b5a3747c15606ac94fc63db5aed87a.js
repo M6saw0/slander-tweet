9bf04fdb119f5802a918cfcd31963167d2fb3b95
Masataka Suzuki <masa.9729taka@gmail.com>
2020-10-04 09:30:07 +0900

    Updates

---
+++ precache-manifest.05b5a3747c15606ac94fc63db5aed87a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372f7ae1cd2c767bd72f9c90055d95b2",
+    "url": "/slander-tweet/index.html"
+  },
+  {
+    "revision": "f29d9fb21cadfdad6f4a",
+    "url": "/slander-tweet/static/css/main.514148c4.chunk.css"
+  },
+  {
+    "revision": "86faf92f1a28fec44edc",
+    "url": "/slander-tweet/static/js/2.c24dfbcf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/slander-tweet/static/js/2.c24dfbcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29d9fb21cadfdad6f4a",
+    "url": "/slander-tweet/static/js/main.e555b453.chunk.js"
+  },
+  {
+    "revision": "bcf82b95006b184b5e05",
+    "url": "/slander-tweet/static/js/runtime-main.59d0d256.js"
+  }
+]);
